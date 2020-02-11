ff60becde9a3460e76e366ba98d84cdf8aeee086
anshukumar314 <60930135+anshukumar314@users.noreply.github.com>
2020-02-11 09:16:25 -0800

    file updated

--- report javascript/README.md
+++ report javascript/README.md
@@ -4,3 +4,11 @@
 
 <p>we will use html,css, & javascript. we will do coding from starting.
  first we made a simple hello world program in javascript.</p>
+<p>
+JavaScript can "display" data in different ways:
+
+Writing into an HTML element, using innerHTML.
+Writing into the HTML output using document.write().
+Writing into an alert box, using window.alert().
+Writing into the browser console, using console.log()
+</p>
--- report javascript/program.js
+++ report javascript/program.js
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+
+<script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script>
+
+</head>
+
 <body>
 
 <h2>What Can JavaScript Do?</h2>
 
+
+
 <p id="demo">JavaScript can change HTML content.</p>
 
 <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+
+<p id="demo">JavaScript can change the style of an HTML element.</p>
+
+<button type="button" onclick="document.getElementById('demo').style.fontSize='35px'">Click Me!</button>
+
+<p>Never call document.write after the document has finished loading.
+It will overwrite the whole document.</p>
+
+<script>
+document.write(5 + 6);
+</script>
 
 </body>
 </html>
